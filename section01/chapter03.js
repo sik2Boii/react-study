0d9dc40911bb77080458ea563e921ebc1b98a9f2
sik2Boii <junsik213@naver.com>
2024-08-07 21:42:27 +0900

    JavaScript 자료형

---
+++ section01/chapter03.js
@@ -0,0 +1,37 @@
+// 1. Number Type
+// 숫자형 변수를 선언하고 값을 할당합니다.
+let num1 = 1;
+let num2 = 2.2;
+let num3 = -3;
+
+// 무한대와 음의 무한대를 나타냅니다.
+let inf = Infinity;
+let mInf = -Infinity;
+
+// 숫자가 아님을 나타내는 NaN 값을 할당합니다.
+let nan = NaN;
+
+// 2. String Type
+// 문자열 변수를 선언하고 값을 할당합니다.
+let firstName = "준식";
+let lastName = "임";
+let myLocation = "부산";
+
+// 문자열을 결합합니다.
+let myName = lastName + firstName;
+
+// 템플릿 리터럴 문법을 사용하여 문자열을 만듭니다.
+let introduceText = `${myName}은 ${myLocation}에 거주합니다.`
+
+// 3. Boolean Type
+// 불리언 변수를 선언하고 값을 할당합니다.
+let isSwithOn = true;
+let isSwithOff = false;
+
+// 4. Null Type
+// null 값을 할당하여 변수를 초기화합니다.
+let empty = null;
+
+// 5. Undefined Type
+// 변수를 선언만 하고 값을 할당하지 않아 undefined 상태로 둡니다.
+let none;
