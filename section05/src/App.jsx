07aa0195e2e6108bf51d5895b6b889f6d7d727a9
Junsik <junsik213@naver.com>
2024-08-13 04:12:00 +0900

    React 컴포넌트

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Main from './components/main'
 
+// 부모 컴포넌트(루트 컴포넌트)
+// 모든 자식 컴포넌트를 포함하는 최상위 컴포넌트입니다.
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
