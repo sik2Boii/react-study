66204f6c490c7b2bfed12c64b857c4c8318cf1a5
Junsik <junsik213@naver.com>
2024-08-12 23:37:24 +0900

    Node.js 라이브러리 사용하기

--- section03/src/index.js
+++ section03/src/index.js
@@ -9,6 +9,13 @@
 // import { add, sub } from "./math.js"; // 명시적으로 내보낸 함수 가져오기
 import mul, { add, sub } from "./math.js";
 
-console.log(add(1, 2)); // 결과: 3
-console.log(sub(1, 2)); // 결과: -1
-console.log(mul(2, 3)); // 결과: 6
+// console.log(add(1, 2)); // 결과: 3
+// console.log(sub(1, 2)); // 결과: -1
+// console.log(mul(2, 3)); // 결과: 6
+
+// 외부 라이브러리 가져오기 (경로가 아닌 라이브러리 이름을 사용)
+// -> 외부 라이브러리를 가져올 때는 경로 대신 라이브러리 이름을 명시합니다.
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color); // 결과: #9a1ccc (생성된 랜덤 색상을 출력)
