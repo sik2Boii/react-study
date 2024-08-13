6864cf683d211bf99b396ecebbcd72093fb0ac38
Junsik <junsik213@naver.com>
2024-08-14 01:06:16 +0900

    React State로 중복된 상태 관리 코드 통합하기

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -7,41 +7,48 @@ import { useState } from "react";
 // 4. 자기소개 입력 필드
 
 const Register = () => {
-  // 각각의 입력 필드에 대한 상태 관리
-  const [name, setName] = useState("이름"); // 이름 상태 변수와 업데이트 함수
-  const [birth, setBirth] = useState(""); // 생년월일 상태 변수와 업데이트 함수
-  const [country, setCountry] = useState(""); // 국적 상태 변수와 업데이트 함수
-  const [bio, setBio] = useState(""); // 자기소개 상태 변수와 업데이트 함수
+  // 모든 입력 필드를 객체 하나로 상태 관리
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
 
-  // 입력 필드의 값이 변경될 때 상태를 업데이트하는 함수들
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-  const onChangeBirth = (e) => {
-    setBirth(e.target.value);
-  };
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value);
-  };
-  const onChangeBio = (e) => {
-    setBio(e.target.value);
+  // 입력 필드의 값이 변경될 때 상태를 업데이트하는 함수
+  // 이벤트가 발생한 입력 필드의 이름과 값을 사용해 상태를 업데이트
+  const onChange = (e) => {
+    setInput({
+      ...input, // 기존 상태를 복사하고
+      [e.target.name]: e.target.value, // 변경된 필드만 업데이트
+    });
   };
 
   return (
     <div>
       {/* 이름 입력 필드 */}
       <div>
-        <input value={name} onChange={onChangeName} placeholder={"이름"} />
+        <input
+          name="name"
+          value={input.name}
+          onChange={onChange}
+          placeholder={"이름"}
+        />
       </div>
 
       {/* 생년월일 입력 필드 */}
       <div>
-        <input value={birth} onChange={onChangeBirth} type="date" />
+        <input
+          name="birth"
+          value={input.birth}
+          onChange={onChange}
+          type="date"
+        />
       </div>
 
       {/* 국적 선택 필드 */}
       <div>
-        <select value={country} onChange={onChangeCountry}>
+        <select name="country" value={input.country} onChange={onChange}>
           <option value=""></option>
           <option value="kr">한국</option>
           <option value="us">미국</option>
@@ -51,7 +58,7 @@ const Register = () => {
 
       {/* 자기소개 입력 필드 */}
       <div>
-        <textarea value={bio} onChange={onChangeBio} />
+        <textarea name="bio" value={input.bio} onChange={onChange} />
       </div>
     </div>
   );
