e6de6734095094a2defca79b940f0d2414a7ea61
Junsik <junsik213@naver.com>
2024-08-14 01:30:47 +0900

    React useRef로 컴포넌트의 변수 생성하기

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 // 간단한 회원가입 폼
 // 1. 이름 입력 필드
@@ -15,13 +15,33 @@ const Register = () => {
     bio: "",
   });
 
+  // useRef를 사용하여 리렌더링 없이 상태를 저장하는 변수 선언
+  // countRef.current로 접근하며, 값이 변경되어도 컴포넌트는 리렌더링되지 않음
+  const countRef = useRef(0); // {current: 0}
+
+  // 일반 자바스크립트 변수 사용
+  // 컴포넌트가 리렌더링될 때마다 초기화되기 때문에 지속적인 값 저장이 불가능
+  let count = 0;
+
+  // 특정 DOM 요소에 직접 접근하기 위한 ref 선언
+  const inputRef = useRef();
+
   // 입력 필드의 값이 변경될 때 상태를 업데이트하는 함수
   // 이벤트가 발생한 입력 필드의 이름과 값을 사용해 상태를 업데이트
   const onChange = (e) => {
+    countRef.current++; // ref의 current 값을 증가시키지만 리렌더링은 발생하지 않음
+    console.log(countRef.current);
     setInput({
       ...input, // 기존 상태를 복사하고
       [e.target.name]: e.target.value, // 변경된 필드만 업데이트
     });
+  };
+
+  const onsubmit = () => {
+    // 이름이 입력되지 않았다면 이름 입력 필드에 포커스를 설정
+    if (input.name === "") {
+      inputRef.current.focus(); // inputRef를 통해 DOM 요소에 직접 접근하여 포커스 설정
+    }
   };
 
   return (
@@ -29,6 +49,7 @@ const Register = () => {
       {/* 이름 입력 필드 */}
       <div>
         <input
+          ref={inputRef} // inputRef를 사용해 이 DOM 요소에 접근
           name="name"
           value={input.name}
           onChange={onChange}
@@ -60,6 +81,8 @@ const Register = () => {
       <div>
         <textarea name="bio" value={input.bio} onChange={onChange} />
       </div>
+
+      <button onClick={onsubmit}>제출</button>
     </div>
   );
 };
