ee01f568d27e39e09794cb6efb08b461096a738e
Junsik <junsik213@naver.com>
2024-08-14 20:30:39 +0900

    Counter App UI 구현하기

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import Controller from "./components/Controller";
+import Viewer from "./components/Viewer";
 
 function App() {
-  return <>Counter App</>;
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer />
+      </section>
+      <section>
+        <Controller />
+      </section>
+    </div>
+  );
 }
 
 export default App;
