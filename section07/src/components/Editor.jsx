943f272bb161e9a3868ab96a71bdf416cf0c7f6b
Junsik <junsik213@naver.com>
2024-08-18 02:22:47 +0900

    To-Do List App To-Do 추가하기

--- section07/src/components/Editor.jsx
+++ section07/src/components/Editor.jsx
@@ -1,10 +1,41 @@
+import { useRef, useState } from "react";
 import "./Editor.css";
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  // Enter 키를 누를 때 onSubmit 함수를 호출하는 함수
+  const onKeydown = (e) => {
+    // Enter 키의 keyCode는 13
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
   return (
     <div className="Editor">
-      <input placeholder="새로운 To-Do..." />
-      <button>추가</button>
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeydown}
+        onChange={onChangeContent}
+        placeholder="새로운 To-Do..."
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   );
 };
