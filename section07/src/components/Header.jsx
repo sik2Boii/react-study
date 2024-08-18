0b2d1d6a6b396737653663d745ad60a77f78d8db
Junsik <junsik213@naver.com>
2024-08-18 18:03:54 +0900

    React React.memo와 컴포넌트 렌더링 최적화

--- section07/src/components/Header.jsx
+++ section07/src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import "./Header.css";
 
 const Header = () => {
@@ -9,4 +10,9 @@ const Header = () => {
   );
 };
 
-export default Header;
+// const memoizedHeader = memo(Header);
+
+// React.memo를 사용하여 Header 컴포넌트의 불필요한 리렌더링을 방지합니다.
+// Header 컴포넌트는 외부에서 전달되는 Props가 없으므로, 매번 동일한 내용을 렌더링합니다.
+// React.memo를 사용하면 상태나 Props가 변경되지 않는 한 컴포넌트를 다시 렌더링하지 않으므로 성능을 최적화할 수 있습니다.
+export default memo(Header);
--- section07/src/components/ToDoItem.jsx
+++ section07/src/components/ToDoItem.jsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import "./ToDoItem.css";
 
 const ToDoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
@@ -24,4 +25,17 @@ const ToDoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   );
 };
 
-export default ToDoItem;
+// 객체 타입은 주소값으로 비교되기 때문에, 리렌더링 시 모든 함수들이 새로운 주소값을 가지게 되어 동일한 값이라도 다른 값으로 인식됩니다.
+// 이로 인해 useMemo가 예상과 다르게 동작할 수 있습니다.
+// 이런 경우 useCallback을 사용하거나, 주소값이 아닌 실제 값을 비교해야 합니다.
+export default memo(ToDoItem, (prevProps, nextProps) => {
+  // 반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단합니다.
+  // true를 반환하면 Props가 변경되지 않았음을 의미하며, 리렌더링을 방지합니다.
+  // false를 반환하면 Props가 변경되었음을 의미하며, 리렌더링이 발생합니다.
+  if (prevProps.id !== nextProps.id) return false;
+  if (prevProps.isDone !== nextProps.isDone) return false;
+  if (prevProps.content !== nextProps.content) return false;
+  if (prevProps.date !== nextProps.date) return false;
+
+  return true;
+});
